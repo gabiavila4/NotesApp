57861847f18306174a02014e7010c37aa101ec86
Olivia Mendez <olivianevaeh@yahoo.com>
2023-11-15 09:49:28 -0600

    deleted things added outlets and vars

--- NotesApp/ViewController.swift
+++ NotesApp/ViewController.swift
@@ -5,14 +5,40 @@
 //  Created by GABRIELA AVILA on 11/14/23.
 //
 
+
+class AppData{
+   // static var month : String
+    
+  //  static var day : String
+    
+  //  static var year : String
+    
+}
+
 import UIKit
 
-var num = 0
-var random = 11
-var rand = ""
+var jan = 31
+var feb = 28
+var mar = 31
+var apr = 30
+var may = 31
+var jun = 30
+var july = 31
+var aug = 31
+var sep = 30
+var oct = 31
+var nov = 30
+var dec = 31
+
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var monthOutlet: UITextField!
+    
+    @IBOutlet weak var dayOutlet: UITextField!
+    
+    @IBOutlet weak var yearOutlet: UITextField!
+    
+    
     @IBOutlet weak var labelOutlet: UILabel!
     
     override func viewDidLoad() {
@@ -20,11 +46,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-/// woah
-    ///
-    /// hello qorld
-    ///
-    /// new comment
-    /// lol
+    @IBAction func submitAction(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+    
+    
 }
 

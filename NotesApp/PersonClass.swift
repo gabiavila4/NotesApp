6a064ea905fc28706a9c175825773167017c676e
Olivia Mendez <olivianevaeh@yahoo.com>
2023-11-16 09:25:43 -0600

    perf

---
+++ NotesApp/PersonClass.swift
@@ -0,0 +1,23 @@
+//
+//  PersonClass.swift
+//  NotesApp
+//
+//  Created by OLIVIA MENDEZ on 11/16/23.
+//
+
+import Foundation
+
+class Person{
+    
+    var month : String
+    var year : String
+    var day : String
+    
+    init(month: String, year: String, day: String) {
+        self.month = month
+        self.year = year
+        self.day = day
+    }
+    
+    
+}
